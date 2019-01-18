467970c099bf6fd0a41da5226f541da1b478818c
donhua <suvorov.mo@yandex.ru>
2019-01-18 23:19:27 +0300

    001

--- tic_tac_toi/main.py
+++ tic_tac_toi/main.py
@@ -1,5 +1,32 @@
 #!/usr/bin/env python
 # -*- coding=utf-8 -*-
 
-import pygame
+import pygame as pg
+import rundom as rnd
 
+pg.init()
+
+
+black    = (   0,   0,   0)
+white    = ( 255, 255, 255)
+green    = (   0, 255,   0)
+red      = ( 255,   0,   0)
+
+text01 = "TicTacToy Game" 
+
+
+size=[700,500]
+screen=pg.display.set_mode(size)
+pg.display.set_caption(text01)
+done=False
+clock=pg.time.Clock()
+ 
+# -------- Основной цикл программы -----------
+while done==False:
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            done=True
+    screen.fill(white)
+    pg.display.flip()
+    
+    clock.tick(20)
