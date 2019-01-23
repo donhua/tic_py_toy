068ba9573cd64b12430eeb9465c26c205123783e
donhua <suvorov.mo@yandex.ru>
2019-01-23 21:00:18 +0300

    delete env

--- tic_tac_toi/main.py
+++ tic_tac_toi/main.py
@@ -3,34 +3,26 @@
 
 import pygame as pg
 
+import const
 pg.init()
 
-
-black    = (   0,   0,   0)
-white    = ( 255, 255, 255)
-green    = (   0, 255,   0)
-red      = ( 255,   0,   0)
-
-text01 = "TicTacToy Game" 
-
-
-size=[700,500]
-screen=pg.display.set_mode(size)
-pg.display.set_caption(text01)
-done=False
-clock=pg.time.Clock()
+screen = pg.display.set_mode([const.win_size["ширина"], const.win_size["высота"])
+pg.display.set_caption(const.tx001)
+done = False
+clock = pg.time.Clock()
  
 # -------- Основной цикл программы -----------
-while done==False:
+while done == False:
     for event in pg.event.get():
         if event.type == pg.QUIT:
-            done=True
-    screen.fill(white)
+            done = True
+    screen.fill(const.color["белый"])
 
     font = pg.font.SysFont('Calibri', 25, True, False)
-    text = font.render("My text", True, black)
-    screen.blit(text, [250,250])
+    text = font.render("My text", True, const.color["черный"])
+    screen.blit(text, [250, 250])
 
     pg.display.flip()
     
-    clock.tick(20)
+    clock.tick(const.fps)
+    
